2c00a86f1f363030054c71ad5cca1abfb0af8c9d
Csongor Gyuricza <csongor.gyuricza@powerreviews.com>
2014-11-02 23:28:24 -0800

    Removed unnecessary begin clauses, removed interrupt rescue

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ require 'yaml'
 require 'digest/md5'
 
 BASE_PATH = File.expand_path(File.dirname(__FILE__))
-SLEEP_DURATION = 60 # seconds
+SLEEP_DURATION = 3600 # seconds
 
 # Load the local config file
 def config
@@ -22,38 +22,29 @@ log.level = Logger::INFO
 
 while true
   begin
-    begin
-      log.info('Started processing queries')
-      config['queries'].each do |query|
-        datafile = "#{BASE_PATH}/#{Digest::MD5.hexdigest(query['source'] + query['regex'])}.data"
-        log.info("Searching for #{query['summary']}")
-        begin
-          SimpleRSS.parse(open(query['source']).read.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')).channel.items.each do |post|
-            title = post.title[/.*\)/] # skip unicode chars after the last parenthesis
-            if title =~ Regexp.new(query['regex'], Regexp::IGNORECASE)
-              known_urls = File.readlines(datafile).collect(&:strip) rescue known_urls = []
-              if not known_urls.include?(post.link.strip) # If this is a new url...
-                log.info('Found new post matching criteria - sending email...')
-                File.open(datafile, 'a').puts(post.link.strip)
-                gmail = Gmail.connect(config['gmail']['account'], config['gmail']['password'])
-                gmail.deliver do
-                  to(query['email'])
-                  subject("#{query['summary']} => #{title}")
-                  body(post.link)
-                end
-                gmail.logout
-              end
+    log.info('Started processing queries')
+    config['queries'].each do |query|
+      datafile = "#{BASE_PATH}/#{Digest::MD5.hexdigest(query['source'] + query['regex'])}.data"
+      log.info("Searching for #{query['summary']}")
+      SimpleRSS.parse(open(query['source']).read.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')).channel.items.each do |post|
+        title = post.title[/.*\)/] # skip unicode chars after the last parenthesis
+        if title =~ Regexp.new(query['regex'], Regexp::IGNORECASE)
+          known_urls = File.readlines(datafile).collect(&:strip) rescue known_urls = []
+          if not known_urls.include?(post.link.strip) # If this is a new url...
+            log.info('Found new post matching criteria - sending email...')
+            File.open(datafile, 'a').puts(post.link.strip)
+            gmail = Gmail.connect(config['gmail']['account'], config['gmail']['password'])
+            gmail.deliver do
+              to(query['email'])
+              subject("#{query['summary']} => #{title}")
+              body(post.link)
             end
+            gmail.logout
           end
-        rescue StandardError => e
-          log.error(e.message)
         end
       end
-      log.info('Finished processing')
     end
-    sleep SLEEP_DURATION
-  rescue SignalException => e
-    log.info('Exiting the program')
-    exit
+    log.info('Finished processing')
   end
+  sleep SLEEP_DURATION
 end
